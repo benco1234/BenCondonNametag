e7c32a746fa333cd5b907a8ff34fede8c89ad722
benco <benco@192.168.20.255>
2023-09-18 14:42:39 -0400

    centered my name on the block
    Teacher told me to chnage the code to have it centered on my nametag

--- BenCondonNametag.groovy
+++ BenCondonNametag.groovy
@@ -1,13 +1,33 @@
 // code here
 
+CSG text = CSG.text("Ben",5)
+
+CSG rotatedText = text.rotz(90)
+
+double distanceyCenter = rotatedText.getCenterY()
+
+println "Center of World "+distanceyCenter
+
+CSG centeredText = rotatedText.movey(-distanceyCenter)
+
 double width = 30
 double depth = 70
 double height = 3
+double widthOfText = centeredText.getTotalY()+4.0
 
+double scale = depth/widthOfText
+
+CSG scaledText = centeredText.scaley(scale)
+
+double distanceInX = scaledText.getCenterX()
+CSG textCenteredInX = scaledText.movex(-distanceInX)
+
+println "Scale for the text "+scale
+println "Total text width is "+widthOfText
 println "Width is "+width
 println "Height is "+height
 println "depth is "+depth
 
 CSG nameTagBase = new Cube(width,depth,height) . toCSG()
 
-return nameTagBase
+return [nameTagBase,textCenteredInX]
